d57751245b74bda889bbc90f263a28491236b9b0
puauksonboon-lang <puaukson.boon@bumail.net>
2025-10-21 14:49:48 +0700

    newoj

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Ant : Enemy
+public class Rock : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
